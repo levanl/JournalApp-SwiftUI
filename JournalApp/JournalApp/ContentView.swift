1aa5a307a32214239f8a7fd3782c9a42d7638b31
Levan Loladze <levan.loladzew@gmail.com>
2023-12-20 14:29:01 +0400

    first commit

---
+++ JournalApp/JournalApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JournalApp
+//
+//  Created by Levan Loladze on 20.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JournalApp/JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Levan Loladze on 20.12.23.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
